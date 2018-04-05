44e73c231d18460169464a5ac536fbedbc99987a
linntunberg <linn.tunberg@gmail.com>
2018-04-05 14:22:21 +0200

    Changes

--- Controllers/CheckoutController.cs
+++ Controllers/CheckoutController.cs
@@ -1,20 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Webshop.Models;
-
+using MySql.Data.MySqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Webshop.Controllers
 {
     public class CheckoutController : Controller
     {
+        private readonly string connectionString;
         // GET: /<controller>/
-        public IActionResult Index()
+        public ActionResult Index()
         {
-            return View();
+            var cartId = GetOrCreateCartId();
+
+            using (var connection = new MySqlConnection(this.connectionString))
+            {
+                try
+                {
+                    var cartItems = connection.Query<CheckoutViewModel>("SELECT carts.cartId, sum(carts.quantity) as quantity, carts.productId, products.price, products.item, products.image FROM products INNER JOIN carts ON carts.productId = products.id WHERE carts.cartId = @cartId GROUP BY carts.productId; ",
+                            new { cartId }).ToList();
+                    return View(cartItems);
+                }
+                catch (Exception)
+                {
+                    return NotFound();
+                }
+            }
+        }
+
+        public CheckoutController(IConfiguration configuration)
+        {
+            this.connectionString = configuration.GetConnectionString("ConnectionString");
+        }
+
+        private string GetOrCreateCartId()
+        {
+            if (this.Request.Cookies.ContainsKey("CartId"))
+            {
+                return this.Request.Cookies["CartId"];
+            }
+            var cartId = Guid.NewGuid().ToString();
+            this.Response.Cookies.Append("CartId", cartId);
+            return cartId;
         }
     }
 }
--- Models/CheckoutViewModel.cs
+++ Models/CheckoutViewModel.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Webshop.Models
 {
     public class CheckoutViewModel
     {
+        public string CartId { get; set; }
+        public int Quantity { get; set; }
+        public int ProductId { get; set; }
+        public int Price { get; set; }
+        public string Item { get; set; }
+        public string Image { get; set; }
     }
 }
