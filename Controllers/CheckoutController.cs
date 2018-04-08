de54e2710c48420e221acfd72c98c140d3f1ed2b
linntunberg <linn.tunberg@gmail.com>
2018-04-08 18:17:11 +0200

    Added things

--- Controllers/CheckoutController.cs
+++ Controllers/CheckoutController.cs
@@ -40,6 +40,53 @@ namespace Webshop.Controllers
             }
         }
 
+        [HttpPost]
+        public ActionResult Order(string FirstName, string LastName, string Email, string Address, string Country, int Zipcode, int CardNumber, string CartId, int Sum)
+        {
+            using (var connection = new MySqlConnection(this.connectionString))
+            {
+
+                try
+                {
+
+                    connection.Execute(
+                        "INSERT INTO Orders(FirstName, LastName, Email, Address, Country, Zipcode, CardNumber, Sum, CartId) VALUES(@firstname, @lastname, @email, @address, @country, @zipcode, @cardnumber, @sum, @cartId)",
+                        new { FirstName, LastName, Email, Address, Country, Zipcode, CardNumber, CartId, Sum });
+
+
+                    var orderInfo = connection.Query<OrderViewModel>("Select * from Orders where Orders.CartId = @CartId;",
+                                                                 new { CartId }).ToList();
+
+
+                    var checkoutItems = connection.Query<CartViewModel>(
+                        "SELECT carts.CartId, Sum(carts.quantity) as quantity, carts.productId, products.price, products.item, products.image FROM products INNER JOIN carts ON carts.productId = products.id WHERE carts.cartId = @cartId GROUP BY carts.productId;",
+                        new { CartId }).ToList();
+
+                    var orderId = orderInfo[0].Id;
+
+                    foreach (var item in checkoutItems)
+                    {
+
+                        connection.Execute(
+                            "INSERT INTO OrderRows(OrderId, ProductId, Item, Price, Quantity) VALUES(@orderId, @productId, @item, @price, @quantity)",
+                            new { orderId, item.ProductId, item.Item, item.Price, item.Quantity });
+                    }
+
+                    
+
+                }
+                catch (Exception)
+                {
+                    return NotFound();
+                }
+
+            }
+            this.Response.Cookies.Delete("CartId");
+
+            return RedirectToAction("Index", "Products");
+        }
+
+
         public CheckoutController(IConfiguration configuration)
         {
             this.connectionString = configuration.GetConnectionString("ConnectionString");
